1716ffc7fc30d2ff80fc1bfe565d53bdc35c506a
22-pig <845501558@qq.com>
2021-09-28 22:10:04 +0800

    笔记

--- chap2/test.py
+++ chap2/test.py
@@ -1,3 +1,14 @@
-a, b = input().split()
-a, b = int(a), int(b)
-print(a + b)
+""" s = input()
+if s[0] == '$':
+    c = eval(s[1:])
+    print("￥{:.2f}".format(7 * c))
+elif s[0] == '￥':
+    f = eval(s[1:])
+    print("${:.2f}".format(f / 7))
+else:
+    print("输入格式错误") """
+
+# str.format()
+x = 3.14159
+y = 2 * x * 3
+print('{0:.2f}' '{1:.2f}'.format(x, y))
